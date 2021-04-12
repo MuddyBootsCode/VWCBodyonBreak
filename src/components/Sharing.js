d53e3a53699df909a92abc4a8f70fd32f038e4bf
Thomas Fitzgerald <fitzgerald.thomas.e@gmail.com>
2021-04-12 14:21:55 -0500

    positioning updates

--- src/components/Sharing.js
+++ src/components/Sharing.js
@@ -5,7 +5,7 @@ import sharing from '../images/sharing.svg'
 const SharingInfoBlock = () => {
   return (
     <div className="SharingInfoBlock">
-        <img src={sharing} alt="person working out" style={{height:'500px', width:'500px'}}/>
+        <img src={sharing} alt="person working out" style={{height:'300px', width:'300px'}}/>
             <div>
                 <h2>Sharing</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae delectus velit voluptatum enim nam. Nobis soluta iure unde neque temporibus assumenda rerum quo eos, ducimus voluptatibus voluptatum ad itaque porro.</p>
--- src/components/Training.js
+++ src/components/Training.js
@@ -6,7 +6,7 @@ const TrainingInfoBlock = () => {
   return (
     
     <div className="TrainingInfoBlock slide">
-        <img src={working_out} alt="person working out" style={{height:'500px', width:'500px'}}/>
+        <img src={working_out} alt="person working out" style={{height:'300px', width:'300px'}}/>
             <div>
                 <h2>Training</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae delectus velit voluptatum enim nam. Nobis soluta iure unde neque temporibus assumenda rerum quo eos, ducimus voluptatibus voluptatum ad itaque porro.</p>
