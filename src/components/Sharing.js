0133bf20cc7a922c3d2d7ad14b45f74fb759222e
Thomas Fitzgerald <fitzgerald.thomas.e@gmail.com>
2021-04-12 16:17:45 -0500

    text update

--- src/components/Sharing.js
+++ src/components/Sharing.js
@@ -8,7 +8,7 @@ const SharingInfoBlock = () => {
         <img src={sharing} alt="person working out" style={{height:'300px', width:'300px'}}/> {/*placeholder svg image*/}
             <div>
                 <h2>Sharing</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae delectus velit voluptatum enim nam. Mr. Curl is a hoe. unde neque temporibus assumenda rerum quo eos, ducimus voluptatibus voluptatum ad itaque porro.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae delectus velit voluptatum enim nam. Mr. Curl is a hoe. Unde neque temporibus assumenda rerum quo eos, ducimus voluptatibus voluptatum ad itaque porro.</p>
             </div>
     </div>
   );
--- src/components/Tracking.js
+++ src/components/Tracking.js
@@ -8,7 +8,7 @@ const TrackingInfoBlock = () => {
         <img src={tracking} alt="person working out" style={{height:'300px', width:'300px'}}/> {/*placeholder svg image*/}
             <div>
                 <h2>Tracking</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae delectus velit voluptatum enim nam. Mr. Curl is a hoe. unde neque temporibus assumenda rerum quo eos, ducimus voluptatibus voluptatum ad itaque porro.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae delectus velit voluptatum enim nam. Mr. Curl is a hoe. Unde neque temporibus assumenda rerum quo eos, ducimus voluptatibus voluptatum ad itaque porro.</p>
             </div>
     </div>
   );
