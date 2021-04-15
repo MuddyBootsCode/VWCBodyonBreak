4e8374ea409dc66d930f87f330d540382dcfd869
Thomas Fitzgerald <fitzgerald.thomas.e@gmail.com>
2021-04-15 15:41:09 -0500

    HomePage Layout adjustments

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,13 @@ import React from 'react';
 import TrainingInfoBlock from '../components/Training';
 import TrackingInfoBlock from '../components/Tracking';
 import SharingInfoBlock from '../components/Sharing';
+import HomePageVideo from '../components/HomePageVideo';
 
 
 const HomePage = () => {
   return (
     <div className="PageBody">
+        <HomePageVideo />
         <TrainingInfoBlock />
         <TrackingInfoBlock />
         <SharingInfoBlock />
