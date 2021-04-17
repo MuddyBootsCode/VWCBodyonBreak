7aade4d23f6c24632f7eb37ecc5e53928e51905b
j3ffjessie <jejess6125@gmail.com>
2021-04-17 14:52:45 -0500

    Reset Yup schema setup without a max input size.

--- src/utils/GoalsPageSchema.js
+++ src/utils/GoalsPageSchema.js
@@ -3,7 +3,6 @@ import * as Yup from 'yup';
 const GoalsPageSchema = Yup.object().shape({
     goals: Yup.string()
     .min(3, 'Goal Description to short, please give more detail.')
-    .max(50, 'Whoa there killer, going a little overboard with the Goal there!')
     .required('Required')
   });
 
