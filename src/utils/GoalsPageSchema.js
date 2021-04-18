c1d4d5c92351bd3754ce2b397d16458b88f399a6
J3ffJessie <J3ffJessie@users.noreply.github.com>
2021-04-18 14:13:51 -0500

    Fix Yup validation with object destructuring
    Changed validation to fit the object that contains an array so that
    validation should work.

--- src/utils/GoalsPageSchema.js
+++ src/utils/GoalsPageSchema.js
@@ -1,9 +1,8 @@
 import * as Yup from 'yup';
 
 const GoalsPageSchema = Yup.object().shape({
-    goals: Yup.string()
-    .min(3, 'Goal Description to short, please give more detail.')
-    .required('Required')
+    goals: Yup.object().shape({
+    name: Yup.string().min(3, 'Goal too short').max().required('Required')
   });
 
 
