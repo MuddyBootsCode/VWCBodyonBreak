2bbed8dac660faf07d0b2d2504a1edc9d140927b
J3ffJessie <J3ffJessie@users.noreply.github.com>
2021-04-18 14:15:07 -0500

    typo fixes

--- src/utils/GoalsPageSchema.js
+++ src/utils/GoalsPageSchema.js
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 const GoalsPageSchema = Yup.object().shape({
     goals: Yup.object().shape({
     name: Yup.string().min(3, 'Goal too short').max().required('Required')
-  });
-
+  }),
+});
 
   export default GoalsPageSchema;
