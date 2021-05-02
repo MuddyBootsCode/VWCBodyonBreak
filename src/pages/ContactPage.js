ba40a7290c03c091f4bb2bcd199e9dd56730ba3f
Thomas Fitzgerald <fitzgerald.thomas.e@gmail.com>
2021-05-02 08:57:09 -0500

    Contact & Terms pages updated

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,7 +3,9 @@ import React from 'react';
 const ContactPage = () => {
   return (
     <div className="PageBody">
-      The contact page
+      <h1>The contact page</h1>
+      <p>Something like a contact form will go here.</p>
+      <p>Maybe an address or phone number as well.</p>
     </div>
   );
 };
--- src/pages/TermsAndConditions.js
+++ src/pages/TermsAndConditions.js
@@ -3,7 +3,9 @@ import React from 'react';
 const TermsAndConditions = () => {
   return (
     <div className="PageBody">
-      Terms and Conditions
+      <h1>Terms and Conditions</h1>
+      <p>Legal jargen for use of product will go here. </p>
+      
     </div>
   );
 };
