3a8b82775b7d97d5552682d7d15488bfa9c3bf4f
Thomas Fitzgerald <fitzgerald.thomas.e@gmail.com>
2021-05-19 15:15:59 -0500

    working on fixing merge conflicts pt 4

--- src/pages/GoalsPage.js
+++ src/pages/GoalsPage.js
@@ -35,6 +35,14 @@ const GoalsPage = () => {
                 name="goals"
                 render={(arrayHelpers) => (
                   <div>
+                    <button
+                      className="goals-list-add-goal-button"
+                      type="button"
+                      onClick={() => arrayHelpers.push("")}
+                    >
+                      {/* show this when user has removed all goals from the list */}
+                      Add a Goal
+                    </button>
                     {props.values.goals && props.values.goals.length >= 0
                       ? props.values.goals.map((goal, index) => (
                           <div key={index}>
@@ -80,14 +88,6 @@ const GoalsPage = () => {
                           </div>
                         ))
                       : null}
-                    <button
-                      className="goals-list-add-goal-button"
-                      type="button"
-                      onClick={() => arrayHelpers.push("")}
-                    >
-                      {/* show this when user has removed all goals from the list */}
-                      Add a Goal
-                    </button>
                     <button type="submit">Submit</button>
                   </div>
                 )}
