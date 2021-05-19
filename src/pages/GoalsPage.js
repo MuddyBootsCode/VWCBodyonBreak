d9abfc1ad61ab7210cf5bfdea681ea7f41e47483
Thomas Fitzgerald <fitzgerald.thomas.e@gmail.com>
2021-05-19 18:08:36 -0500

    Move add goal button

--- src/pages/GoalsPage.js
+++ src/pages/GoalsPage.js
@@ -1,55 +1,53 @@
-import React, {useState} from 'react';
-import {Formik, Form, Field, FieldArray, setIn} from 'formik';
-import GoalsPageSchema from '../utils/GoalsPageSchema';
-import { isNonEmptyArray } from '@apollo/client/utilities';
+import React, { useState } from "react";
+import { Formik, Form, Field, FieldArray, setIn } from "formik";
+import GoalsPageSchema from "../utils/GoalsPageSchema";
+import { isNonEmptyArray } from "@apollo/client/utilities";
 // ErrorMessage can be used, need to look into it more to figure out how
 
-
-
-
-
 const GoalsPage = () => {
-  const initialGoals = { goals:
-      [
-        'Greg is Awesome',
-        'Fitz is cool',
-        'Michael is Russian',
-        "Jeff's shoulder is trashed"
-      ]
+  const initialGoals = {
+    goals: [
+      "Greg is Awesome",
+      "Fitz is cool",
+      "Michael is Russian",
+      "Jeff's shoulder is trashed",
+    ],
   };
-  const [updatedGoalIndex, setUpdateGoalIndex] = useState(null)
+  const [updatedGoalIndex, setUpdateGoalIndex] = useState(null);
 
-    return (
-      <div className= "PageBody">
-        <h1> Goals List </h1>
-        <Formik
-          initialValues={initialGoals}
-          validationSchema={GoalsPageSchema}
-          onSubmit={( values, actions) => {
-            const { setSubmitting } = actions;
-            setTimeout(() => {
-              setSubmitting(false)
-            }, 500);
-          }}
-        >
-          {(props) => {
-            return(
-              <Form onSubmit={props.handleSubmit}>
-                <FieldArray
-                  name="goals"
-                  render={(arrayHelpers) => (
-                    <div>
-                      <button className="goals-list-add-goal-button" type="button" onClick={() => arrayHelpers.push("")}>
-                        {/* show this when user has removed all goals from the list */}
-                        Add a Goal
-                      </button>
-                      {props.values.goals && props.values.goals.length >= 0 ? (
-                        props.values.goals.map((goal, index) => (
+  return (
+    <div className="PageBody">
+      <h1> Goals List </h1>
+      <Formik
+        initialValues={initialGoals}
+        validationSchema={GoalsPageSchema}
+        onSubmit={(values, actions) => {
+          const { setSubmitting } = actions;
+          setTimeout(() => {
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        {(props) => {
+          return (
+            <Form onSubmit={props.handleSubmit}>
+              <FieldArray
+                name="goals"
+                render={(arrayHelpers) => (
+                  <div>
+                    {props.values.goals && props.values.goals.length >= 0
+                      ? props.values.goals.map((goal, index) => (
                           <div key={index}>
                             <Field name={`goals.${index}`}>
                               {({ field, meta }) => (
-                                <div style={{ display: 'flex'}}>
-                                  <input title="Goal, click to edit" className="goals-list-text-area" type="text" {...field} placeholder="Click to enter new goal."/>
+                                <div style={{ display: "flex" }}>
+                                  <input
+                                    title="Goal, click to edit"
+                                    className="goals-list-text-area"
+                                    type="text"
+                                    {...field}
+                                    placeholder="Click to enter new goal."
+                                  />
                                   <button
                                     title="Remove Goal"
                                     className="remove-goal-button"
@@ -63,32 +61,43 @@ const GoalsPage = () => {
                                     type="button"
                                     disabled={props.isSubmitting}
                                     onClick={() => {
-                                      arrayHelpers.replace(index, props.values.goals[index])
+                                      arrayHelpers.replace(
+                                        index,
+                                        props.values.goals[index]
+                                      );
                                     }}
 
                                     // insert an empty string at a position
                                   >
-                                    {
-                                      props.isSubmitting ? "..." : "Set"
-                                    }
+                                    {props.isSubmitting ? "..." : "Set"}
                                   </button>
-                                  {meta.touched &&
-                                  meta.error && <div className="error">{meta.error}</div>}
+                                  {meta.touched && meta.error && (
+                                    <div className="error">{meta.error}</div>
+                                  )}
                                 </div>
                               )}
                             </Field>
                           </div>
                         ))
-                      ) : null }
-                      <button type="submit">Submit</button>
-                    </div>
-                  )}/>
-              </Form>
-            )
-          }}
-        </Formik>
-      </div>
-    )
+                      : null}
+                    <button
+                      className="goals-list-add-goal-button"
+                      type="button"
+                      onClick={() => arrayHelpers.push("")}
+                    >
+                      {/* show this when user has removed all goals from the list */}
+                      Add a Goal
+                    </button>
+                    <button type="submit">Submit</button>
+                  </div>
+                )}
+              />
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
 };
 
 export default GoalsPage;
